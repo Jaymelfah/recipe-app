b779cc8b1c604716243fb88cd96fc1e646628ca4
unknown <sharonkessio6@gmail.com>
2023-02-14 15:21:43 +0300

    create controller and views

---
+++ app/controllers/food_controller.rb
@@ -0,0 +1,7 @@
+class FoodController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ spec/requests/food_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Foods", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/food/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/food/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
