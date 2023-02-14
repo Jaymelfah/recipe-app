d2beca02500cc3ff893a50fa32907387eb220861
unknown <sharonkessio6@gmail.com>
2023-02-14 17:40:49 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
-  
-  root "foods#index"
-  get 'foods', to: 'foods#index'
-  get '/foods/new', to: 'foods#new', as: 'new_food'
-  get '/foods/:id', to: 'foods#destroy'
-  post 'foods', to: 'foods#create'
-  delete '/foods/:id', to: 'foods#destroy', as: 'food'
-
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'recipes#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'public_recipes#index', as: :unauthenticated_root
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :recipes, only: [:index, :new, :create, :show, :destroy] do
+  resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+  end
+  resources :foods, only: [:index, :new, :create, :show, :destroy] 
+  resources :public_recipes, only: [:index]
 end
+
+
