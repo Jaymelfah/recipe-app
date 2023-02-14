ec3ae65afee6e242f5b5c4d9a4481248de791678
unknown <sharonkessio6@gmail.com>
2023-02-14 17:05:40 +0300

    Add food to databse

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,31 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Foods.all.includes([:user])
+  end
+
+  def new
+    @food = Foods.new
+  end
+
+  def show
+  end
+
+  def create
+    @food = Foods.new(params_foods)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to foods_path
+      flash[:notice] = "Food was Successfully Created"
+    else
+      render :new
+      flash[:notice] = "Food has not been created"
+    end
+  end
+
+  private
+
+  def params_foods
+    params.require(:foods).permit(:name, :measurement_unit, :price, :quantity)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  root "foods#index"
+  get 'foods', to: 'foods#index'
+  get '/foods/new', to: 'foods#new', as: 'new_food'
+  get '/foods/:id', to: 'foods#destroy'
+  post 'foods', to: 'foods#create'
+  delete '/foods/:id', to: 'foods#destroy', as: 'food'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
