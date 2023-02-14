059d04e323663f9aa59b401d95e8ee4ed96d099f
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-14 10:39:22 +0000

    Add styling and recipe foods controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'recipes#index', as: :authenticated_root
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
