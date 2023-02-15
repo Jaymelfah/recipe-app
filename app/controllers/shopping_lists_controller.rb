664a88a41a6c66d5484c50ddd1b4031860ce0de4
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-15 13:17:34 +0000

    Add generation of shopping lists feature

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,20 @@
+class ShoppingListsController < ApplicationController
+  def index
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods.includes([:food])
+    @total = sum(@recipe_foods)
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods.includes([:food])
+  end
+
+  def sum(recipe_foods)
+    total = 0
+    recipe_foods.each do |item|
+      total += item.food.price * item.quantity.to_i
+    end
+    total
+  end
+end
